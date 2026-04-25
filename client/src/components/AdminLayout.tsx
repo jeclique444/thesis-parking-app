0452097b677592534dbff5dc1e69c6f8c63c323f
regi <reginangeli.cadelina@gmail.com>
2026-04-25 13:46:40 +0800

    color

--- client/src/components/AdminLayout.tsx
+++ client/src/components/AdminLayout.tsx
@@ -168,7 +168,7 @@ export default function AdminLayout({ children, title }: AdminLayoutProps) {
             noiseIntensity={0.05}
             scanlineIntensity={0.3}
             scanlineFrequency={800}
-            hueShift={345}
+            hueShift={300}
             warpAmount={0.3}
             resolutionScale={1}
           />
